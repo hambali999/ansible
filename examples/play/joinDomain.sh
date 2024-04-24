d77f98df1785bf99f5a26dc6ce31633cbb8d7ecc
Hambali Nur <chorongbali@gmail.com>
2024-04-24 21:34:41 +0800

    test

---
+++ examples/play/joinDomain.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Specify the inventory file and playbook file
+INVENTORY_FILE="inventory/windows_inventory.yml"
+PLAYBOOK_FILE="playbooks/join_domain.yml"
+
+# Test the playbook
+ansible-playbook -i $INVENTORY_FILE --syntax-check --lint --check $PLAYBOOK_FILE
+
+# Run the playbook
+ansible-playbook -i $INVENTORY_FILE $PLAYBOOK_FILE
